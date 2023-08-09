beb5a20cf827e979d516466d9e8b83540678ce79
Antares-ProgramIng <140986762+Antares-ProgramIng@users.noreply.github.com>
2023-08-09 19:17:41 -0300

    Final Commit

--- js/validacion.js
+++ js/validacion.js
@@ -14,7 +14,7 @@ function CheckFields() {
     let password1 = document.getElementById('password1').value;
     let password2 = document.getElementById('password2').value;
     let chkbx = document.getElementById('terminos');
-    if(name > 0 && lastname > 0 && email > 0 && password1.length > 6 && password2.length > 6 && chkbx.checked && password1 == password2){
+    if(name > 0 && lastname > 0 && email > 0 && password1.length >= 6 && password2.length >= 6 && chkbx.checked && password1 == password2){
         return true;
     }else{
         return false;
